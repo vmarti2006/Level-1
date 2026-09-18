8d501456bf03bacc6a6638e104170357ba48ecd1
victormarti2006 <victormarti2006@gmail.com>
2026-09-18 16:11:26 +0200

    15

---
+++ 3_Programas_Basics/14_El_Calendary_de_dates/Program.cs
@@ -0,0 +1,14 @@
+﻿// Obtindre dades
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Entra la data sense formatar:");
+string data = Console.ReadLine()!;
+
+// Calcul
+string d =$"{data[0]}{data[1]}";
+string m =$"{data[2]}{data[3]}";
+string a =$"{data[4..]}";
+
+// Resultat
+
+Console.WriteLine($"La data és {d}/{m}/{a}");
---
+++ 3_Programas_Basics/15_Treu_lletres/Program.cs
@@ -0,0 +1,24 @@
+﻿// Intro 
+Console.WriteLine("Paraula:");
+string paraula = Console.ReadLine()!;
+
+// Trabajo
+int u = paraula.Length -1;
+int x = paraula.Length;
+if (x % 2 ==0)
+{
+var p_i_p = $"{paraula[0]} * {paraula[u]}";
+// Resultado
+Console.WriteLine($"Resultat: {p_i_p}");
+}
+
+else
+{
+
+int m = u/2;
+
+var p_i_ip= $"{paraula[0]} {paraula[m]} {paraula[u]}";
+
+// Resultado
+Console.WriteLine($"Resultat: {p_i_ip}");
+}
