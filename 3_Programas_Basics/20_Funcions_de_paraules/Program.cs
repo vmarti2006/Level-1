610bb2f4b2f6e3510c820890915fb035a4940c03
victormarti2006 <victormarti2006@gmail.com>
2026-09-19 11:54:24 +0200

    21 i final

--- 3_Programas_Basics/20_Funcions_de_paraules/Program.cs
+++ 3_Programas_Basics/20_Funcions_de_paraules/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Introduccio
+Console.WriteLine("Paraula: ");
+string p = Console.ReadLine()!;
+int x = p.Length;
+
+Console.WriteLine($"La paraula\"{p}\" te {x} caracters");
---
+++ 3_Programas_Basics/21_Gira_les_gifres/Program.cs
@@ -0,0 +1,14 @@
+﻿// info 
+Console.Write("Entra el número a invertir: ");
+string numero_t = Console.ReadLine()!;
+
+// Proces
+char n1 = numero_t[2];
+char n2 = numero_t[1];
+char n3 = numero_t[0];
+
+string numero_t2 = $"{n1}{n2}{n3}";
+int n = Int32.Parse(numero_t2);
+
+// Resultat
+Console.Write($"El número invertit és: {n} ");
