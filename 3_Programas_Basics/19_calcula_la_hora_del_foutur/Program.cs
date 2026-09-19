7175e1879e26179a1230a24cbeea8369f7b916c9
victormarti2006 <victormarti2006@gmail.com>
2026-09-19 11:34:20 +0200

    19

---
+++ 3_Programas_Basics/19_calcula_la_hora_del_foutur/Program.cs
@@ -0,0 +1,19 @@
+﻿// Inici
+Console.WriteLine("Hora actual:");
+var hora_acrual = Console.ReadLine()!;
+int ha = Int32.Parse(hora_acrual);
+
+Console.WriteLine("Hora a incrementar:");
+var hora_incremental = Console.ReadLine()!;
+int hi = Int32.Parse(hora_incremental);
+
+// Calculs
+
+int hf = ha + hi;
+if (hf > 12)
+{
+    hf = hf-12;
+}
+
+// Resultat
+Console.WriteLine($"D'aqui a {hi} hores seran les {hf}");
