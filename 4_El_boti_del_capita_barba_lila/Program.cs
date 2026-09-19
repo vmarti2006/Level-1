f4538f0aba861b25dc3c256a472ec1e7bf3bf90c
victormarti2006 <victormarti2006@gmail.com>
2026-09-19 12:20:05 +0200

    El cpita barba lila

---
+++ 4_El_boti_del_capita_barba_lila/Program.cs
@@ -0,0 +1,21 @@
+﻿// Informacio
+using System.Data;
+
+Console.WriteLine("Numro pirates: ");
+var num_pir = Console.ReadLine()!;
+int np = Int32.Parse(num_pir);
+
+Console.WriteLine("Monedes d'or: ");
+var num_mon = Console.ReadLine()!;
+int nm = Int32.Parse(num_mon);
+
+// Calculs
+
+int rep = nm/np;
+int capita = nm%np;
+
+// int capita2= nm - (rep * np);
+
+// Solucio
+
+Console.WriteLine($"Cada pirate rebra {rep} i el capita {capita} monedes extra");
