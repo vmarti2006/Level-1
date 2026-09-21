5bc1fea7d7977b77cfff848c9b511281b89e73e1
victormarti2006 <victormarti2006@gmail.com>
2026-09-21 18:18:45 +0200

    5 i 7

--- 5_Salbar_la_princesa/Program.cs
+++ 5_Salbar_la_princesa/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Introduccio
+Console.Write("Quants carros ha de preparar? ");
+var carros = Console.ReadLine()!;
+var c = Int32.Parse(carros);
+
+Console.Write("Quin és el preu d'una ferradura? ");
+var ferralla = Console.ReadLine()!;
+var f = Int32.Parse(ferralla);
+
+Console.Write("Quin és el preu d'un sac de menjar? ");
+var sacs = Console.ReadLine()!;
+var s= Int32.Parse(sacs);
+
+// Calcul
+int resultat = c*(f*4) + (s*2)*c;
+
+Console.WriteLine($"Cost total de preparar els carros: {resultat} euros");
