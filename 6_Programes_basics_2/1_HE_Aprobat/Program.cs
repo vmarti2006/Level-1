83690c27af91dca40fbb81ceae2bdcaef8616625
victormarti2006 <victormarti2006@gmail.com>
2026-09-21 18:58:58 +0200

    mañana sigo

---
+++ 6_Programes_basics_2/1_HE_Aprobat/Program.cs
@@ -0,0 +1,19 @@
+﻿// Donar Numeros
+Console.Write("Entra la nota: ");
+var notita= Console.ReadLine()!;
+var nota = Int32.Parse(notita);
+
+// Calcula
+var resultat = CalculaSiAproba(nota);
+
+//resultat
+Console.WriteLine(resultat);
+
+string CalculaSiAproba(int nota)
+{
+   if(nota<5){ return "Has suspes";}
+   
+   return  "Has Aprobat";
+ 
+}
+;
---
+++ 6_Programes_basics_2/2_Ordena_dos_nombres/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Demana Info
+        Console.WriteLine("Número: ");
+        var numero_1 = Console.ReadLine()!;
+        var num_1 = int.Parse(numero_1);
+
+        Console.WriteLine("Número: ");
+        var numero_2 = Console.ReadLine()!;
+        var num_2 = int.Parse(numero_2);
+        // cas 2
+        Console.WriteLine("Número: ");
+        var numero_3 = Console.ReadLine()!;
+        var num_3 = int.Parse(numero_3);
+
+        // Cas 1
+        //Calcula
+        var resultat1 = CalculaQuinVaDevant_1(num_1, num_2);
+
+        // Cas 2
+        // Calcula
+        var resultat2 = CalculaQuinVaDevant_2(num_1, num_2, num_3);
+
+
+        //Resultat
+        Console.WriteLine(resultat1);
+
+        // Cas 1
+        string CalculaQuinVaDevant_1(int num_1, int num_2)
+        {
+            if (num_1 < num_2) { return $"{num_2} {num_1}"; }
+            ;
+            return $"{num_1} {num_2}";
+        }
+
+        // Cas 2
+
+        string CalculaQuinVaDevant_2(int num_1, int num_2, int num_3)
+        {
+            if (num_1 < num_2 && num_2 < num_3){ return $"{num_3} {num_2} {num_1}"; }
+            if (num_2 < num_1 && num_1 < num_3){ return $"{num_3} {num_1} {num_2}"; }
+            if (num_3 < num_2 && num_2 < num_1){ return $"{num_1} {num_2} {num_3}"; }
+            if (num_1 < num_2 &&  num_2< num_3){ return $"{num_1} {num_2} {num_3}"; }
+            ;
+        }
+;
+    }
+}
