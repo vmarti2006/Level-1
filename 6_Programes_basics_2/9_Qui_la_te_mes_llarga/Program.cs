e46025a801bda017684d2431326086d8baf90c81
victormarti2006 <victormarti2006@gmail.com>
2026-09-22 19:35:58 +0200

    Add generated NuGet files and project assets for .NET 10.0
    - Created `9_Qui_la_te_mes_llarga.csproj.nuget.g.props` and `9_Qui_la_te_mes_llarga.csproj.nuget.g.targets` for NuGet package management.
    - Added assembly attributes in `9_Qui_la_te_mes_llarga.AssemblyAttributes.cs` for .NET 10.0.
    - Generated `9_Qui_la_te_mes_llarga.AssemblyInfo.cs` with assembly metadata.
    - Included `9_Qui_la_te_mes_llarga.GeneratedMSBuildEditorConfig.editorconfig` for project configuration.
    - Created `9_Qui_la_te_mes_llarga.GlobalUsings.g.cs` for global using directives.
    - Added `project.assets.json` to manage project dependencies.
    - Included `project.nuget.cache` for NuGet cache management.

---
+++ 6_Programes_basics_2/9_Qui_la_te_mes_llarga/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // info
+        Console.WriteLine("Entra el número:");
+        var paraula1 = Console.ReadLine()!;
+
+        Console.WriteLine("Entra el número:");
+        var paraula2 = Console.ReadLine()!;
+
+        Console.WriteLine("Entra el número:");
+        var paraula3 = Console.ReadLine()!;
+
+        // reultat 
+        var resultat = QuinaEsMesLLarga(paraula1, paraula2, paraula3);
+        Console.WriteLine(resultat);
+
+        //Proces
+
+        string QuinaEsMesLLarga(string paraula1, string paraula2, string paraula3)
+        {
+            string lamesllarga= paraula1;
+            
+            if (lamesllarga.Length<paraula2.Length)
+            {
+                lamesllarga = paraula2;
+            }
+            if (lamesllarga.Length<paraula3.Length)
+            {
+                lamesllarga = paraula3;
+            }
+           
+            if (paraula1.Length==paraula3.Length && paraula3.Length==paraula2.Length){return "les tres son igual de llargues";}
+
+            return $"La paraula més llarga és {lamesllarga}";}
+            
+        }
+    }
