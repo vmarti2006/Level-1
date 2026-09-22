f792bd91165686ce2fdd3ba48fa5f3d2a85b3116
victormarti2006 <victormarti2006@gmail.com>
2026-09-22 19:46:21 +0200

    Add generated NuGet files and project assets for 11_El_Periodista_especial
    - Created project.assets.json to manage package dependencies.
    - Added .NETCoreApp,Version=v10.0 assembly attributes.
    - Included .NET 10.0 specific AssemblyInfo and GlobalUsings files.
    - Generated editorconfig for project settings.
    - Added cache files for assembly info and NuGet project.
    - Created necessary .props and .targets files for NuGet integration.

---
+++ 6_Programes_basics_2/10_El_mes_jove/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Info
+        Console.WriteLine("Entra el primer nom:");
+        string nom1 = Console.ReadLine()!;
+
+        Console.WriteLine($"Entra l'edat d'en {nom1}:");
+        var edat1 = Console.ReadLine()!;
+        var ed_1 = int.Parse(edat1);
+
+        Console.WriteLine("Entra el segon nom:");
+        string nom2 = Console.ReadLine()!;
+
+        Console.WriteLine($"Entra l'edat d'en {nom2}:");
+        var edat2 = Console.ReadLine()!;
+        var ed_2 = int.Parse(edat2);
+
+        // Resultat
+        var resultat = QuiEsElMesJove(nom1, nom2, ed_1, ed_2);
+        Console.WriteLine(resultat);
+
+        // Proces
+        string QuiEsElMesJove(string nom1, string nom2, int ed_1, int ed_2)
+        {
+            if (ed_1<ed_2){return $"En {nom1} és més jove que en {nom2}";}
+            if (ed_2<ed_1){return $"En {nom2} és més jove que en {nom1}";}
+            return "Tenen la mateixa edat";
+        }
+;
+    }
+}
---
+++ 6_Programes_basics_2/11_El_Periodista_especial/Program.cs
@@ -0,0 +1,2 @@
+﻿// Info
+Console.WriteLine("Hello, World!");
