92034cbd552cd3f17cd0b3d904abccb43c564f7a
victormarti2006 <victormarti2006@gmail.com>
2026-09-22 18:45:12 +0200

    Add generated NuGet files and project assets for .NET 10.0
    - Created `6_Inici_sessio_especial.csproj.nuget.g.props` and `6_Inici_sessio_especial.csproj.nuget.g.targets` for NuGet package management.
    - Added assembly attributes and info in `6_Inici_sessio_especial.AssemblyInfo.cs` for versioning and metadata.
    - Generated project assets and cache files including `project.assets.json` and `project.nuget.cache` for dependency resolution.
    - Included global usings in `6_Inici_sessio_especial.GlobalUsings.g.cs` for common namespaces.
    - Created editor config file for project settings and analysis level.

---
+++ 6_Programes_basics_2/5_Nombres_parells_senars/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Informacio
+        Console.WriteLine("Entra el número: ");
+        var numero = Console.ReadLine()!;
+        var num = int.Parse(numero);
+
+        // resultat
+        var resultat = DiguemSiEsParellOSenar(num);
+        Console.WriteLine(resultat);
+
+        //Proces
+        string DiguemSiEsParellOSenar(int num)
+        {
+            if (num % 2 == 0) { return "El número és parell"; }
+            else if (num % 2 != 0) { return "El número és senar"; }
+            return "No es un numero";
+        }
+        ;
+    }
+}
