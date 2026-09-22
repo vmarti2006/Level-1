b962082de52d08bf36a9888a5e55e05ca2ce2084
victormarti2006 <victormarti2006@gmail.com>
2026-09-22 18:35:24 +0200

    Add generated NuGet files and project assets for 4_Calcula_la_edat
    - Created 4_Calcula_la_edat.csproj.nuget.g.props and 4_Calcula_la_edat.csproj.nuget.g.targets for NuGet package management.
    - Added .NETCoreApp,Version=v10.0 assembly attributes and assembly info files.
    - Generated project.assets.json and project.nuget.cache for dependency management.
    - Included global usings and editor config for project settings.
    - Updated assets cache for package references.

---
+++ 6_Programes_basics_2/4_Calcula_la_edat/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // info
+        Console.WriteLine("Posa el teu any de naixement");
+        Console.WriteLine("Any: ");
+        var any_naixement = Console.ReadLine()!;
+        var any_n = int.Parse(any_naixement);
+
+        Console.WriteLine("Mes: ");
+        var mes_naixement = Console.ReadLine()!;
+        var mes_n = int.Parse(mes_naixement);
+
+        Console.WriteLine("dia: ");
+        var dia_naixement = Console.ReadLine()!;
+        var dia_n = int.Parse(dia_naixement);
+
+        // clacul
+        var resultat = CalcualLaMevaEdat(dia_n, mes_n, any_n);
+
+        // Resultat
+        Console.WriteLine(resultat);
+
+        // Proces
+        string CalcualLaMevaEdat(int dia_n, int mes_n, int any_n)
+        {
+            // calcula quin dia mes i any es 
+            var avui = DateTime.Now;
+            var any = avui.Year;
+            var mes = avui.Month;
+            var dia = avui.Day;
+
+            // Calculs de esdat
+            int edat_f=0;
+            int edat_base = any - any_n;
+            if (mes>mes_n)
+            {
+                edat_f=edat_base++;
+                return $"Tens {edat_f} anys";
+            }
+            else if(mes == mes_n && dia_n<=dia)
+            {
+                edat_f=edat_base++;
+                return $"Tens {edat_f} anys";
+            }
+            return $"Tens {edat_base} anys";
+        }
+    }
+}
