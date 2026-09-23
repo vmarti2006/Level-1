8d94296275726bca4ea64c1ab9951eb7bc13154b
victormarti2006 <victormarti2006@gmail.com>
2026-09-23 09:54:27 +0200

    Add generated files for project 15_Triangles_correctes_e_incorrectes
    - Created NuGet props and targets files for package management.
    - Added assembly attributes and info for .NETCoreApp version 10.0.
    - Generated MSBuild editor config and global usings for the project.
    - Included project.assets.json and project.nuget.cache for dependency management.
    - Established cache files for assembly info and assets.

---
+++ 6_Programes_basics_2/14_El_mes_proper/Program.cs
@@ -0,0 +1,32 @@
+﻿// Info
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Entra el número: ");
+var numero1=Console.ReadLine()!;
+var num1= Int32.Parse(numero1);
+
+Console.WriteLine("Entra el número: ");
+var numero2=Console.ReadLine()!;
+var num2= Int32.Parse(numero2);
+
+Console.WriteLine("Entra el número: ");
+var numero3=Console.ReadLine()!;
+var num3= Int32.Parse(numero3);
+
+// resultat 
+var resultat= CuinEsElMesProper(num1,num2,num3);
+Console.WriteLine(resultat);
+
+// Proces
+string CuinEsElMesProper(int num1,int num2,int num3)
+{
+    // Preparacio
+    int suma1 = num1 +num2;
+    int suma2 = num1+num3;
+    
+    // Quin es 
+    if (suma1<suma2){return$"El número més proper a {num1} és el {num2}";}
+    else if (suma1>suma2){return$"El número més proper a {num1} és el {num3}";}
+    return $"Tots dos números són igual de propers a {num1}";
+}
+;
