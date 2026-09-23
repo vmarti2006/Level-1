d663a1a0294f70997d2bb3c33e35eb293be75e5c
victormarti2006 <victormarti2006@gmail.com>
2026-09-23 09:21:12 +0200

    Add generated NuGet files and project assets for 13_El_numero_mes_gran
    - Created project.assets.json to manage project dependencies.
    - Added .csproj.nuget.g.props and .csproj.nuget.g.targets for NuGet integration.
    - Generated assembly attributes and assembly info files for .NET 10.0.
    - Included global usings and editor config for project settings.
    - Updated project.nuget.cache and assets.cache for package management.

---
+++ 6_Programes_basics_2/12_El_casal_de_estiu/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Info 
+        Console.WriteLine("Entra el numero: ");
+        var numero = Console.ReadLine()!;
+        var num = int.Parse(numero);
+
+        // Resultat
+        var resultat = QuinColorSera(num);
+        Console.WriteLine(resultat);
+
+        // Proces
+        string QuinColorSera(int num)
+        {
+            if (num <= 20) { return "Va al grup  vermell"; }
+            else if (num <= 40) { return "Va al grup  blau"; }
+            else if (num <= 60) { return "Va al grup  verd"; }
+            return "Va al grup  blanc";
+        }
+;
+    }
+}
