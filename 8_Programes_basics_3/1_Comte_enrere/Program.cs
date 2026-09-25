bbe5e99ac7434a8a9b90d15c92cd4c9816dc75ac
victormarti2006 <victormarti2006@gmail.com>
2026-09-25 16:46:40 +0200

    Add generated NuGet files and project assets for .NET 10.0
    - Created `2_Els_primers_100_cenars.csproj.nuget.g.props` and `2_Els_primers_100_cenars.csproj.nuget.g.targets` for NuGet package management.
    - Added assembly attributes in `2_Els_primers_100_cenars.AssemblyInfo.cs` for project metadata.
    - Generated `.NETCoreApp,Version=v10.0.AssemblyAttributes.cs` for target framework identification.
    - Included `project.assets.json` to manage project dependencies and assets.
    - Created `2_Els_primers_100_cenars.GlobalUsings.g.cs` for global using directives.
    - Added `2_Els_primers_100_cenars.GeneratedMSBuildEditorConfig.editorconfig` for editor configuration.
    - Updated `project.nuget.cache` for NuGet restore state.

---
+++ 8_Programes_basics_3/1_Comte_enrere/Program.cs
@@ -0,0 +1,20 @@
+﻿// Informacio
+using System.Runtime.CompilerServices;
+
+int deu = 10;
+
+// Proces
+var resultat = ComteEnrrere(deu);
+// Resultat
+Console.WriteLine(resultat);
+
+string ComteEnrrere(int deu)
+{
+    for (int i= 0; i < 10; i++)
+    {
+        Console.WriteLine(deu);
+        deu--;
+    }
+    return"Despege";
+}
+;
