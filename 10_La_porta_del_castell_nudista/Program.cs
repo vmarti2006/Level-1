65ea30aec635afcd731cd6afc6c383f49e3286dd
victormarti2006 <victormarti2006@gmail.com>
2026-09-25 16:31:15 +0200

    Implement main program to validate age for castle entry

--- 10_La_porta_del_castell_nudista/Program.cs
+++ 10_La_porta_del_castell_nudista/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        string ComprobaEdat(int edat, int edatminima)
+        {
+            if (edat < edatminima) { return "Endavant cavaller! Ja pots entrar al castell."; }
+            return "Ho sento cavaller, no pots entrar al castell.";
+        }
+        
+        var edatminima = 18;
+        int edat = 0;
+        int comptador = 0;
+        var resultat="";
+
+        do{
+        try {
+            // Info
+            Console.WriteLine("Quina edat tens? ");
+            var edat_1 = Console.ReadLine()!;
+            edat = int.Parse(edat_1);
+
+            if (edat < 0 || edat > 130)
+            {
+                throw new Exception("Aquesta edat es imposible");
+            }
+            comptador++;
+            //Comprovar la contraseña
+
+            resultat = ComprobaEdat(edat, edatminima);
+
+            //Imprimir resultat
+            Console.WriteLine(resultat);
+        ;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("La edat a de ser un numero");
+        }
+        catch (Exception ex) 
+        { Console.WriteLine(ex.Message); }
+        }
+        while(resultat!="Endavant cavaller! Ja pots entrar al castell." && comptador==1);
+        
+    }
+}
